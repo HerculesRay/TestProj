8934768ba618fb56383e60c57d7f3ab2c3358354
rajiv <rajiv@Dell_Rajiv>
2020-01-27 04:01:58 +0530

    TEST3

---
+++ SeleniumBDD/src/main/java/com/opencart/pages/HomePage3.java
@@ -0,0 +1,38 @@
+package com.opencart.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage3 {
+
+	WebDriver driver;
+	By searchfor = By.name("search");
+	By searchclick = By.xpath("//button/i[@class='fa fa-search']");
+	By seeCart = By.xpath("//div[@id='cart']//i[@class='fa fa-shopping-cart']");
+	private String searchingfor;
+	
+
+
+	public String getSearchingfor() {
+		return searchingfor;
+	}
+
+	public void setSearchingfor(String searchingfor) {
+		this.searchingfor = searchingfor;
+	}
+
+	public HomePage3(WebDriver driver){
+		this.driver = driver;
+	}
+	
+	public void setSearchfor(String searchingfor) {
+		driver.findElement(searchfor).clear();
+		driver.findElement(searchfor).sendKeys(searchingfor);
+		driver.findElement(searchclick).click();
+	}
+
+	public void SeeCart() {
+		driver.findElement(seeCart).click();
+	}
+	
+}
